77769fd4373e885ce85f45598a9dd3bd52c6ec36
MohammadOfficials <mohammadofficial4@gmail.com>
2024-12-30 19:03:19 +0600

    js-loop

---
+++ index.js
@@ -0,0 +1,447 @@
+// let i = 1;
+// let sum = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     sum = sum + i;
+//     console.log('sum', sum);
+//     i++;
+// }
+
+
+// let num = 11;
+// let sum = 0;
+// while (num <= 20) {
+//     console.log(num);
+//     sum = sum + num;
+//     console.log('sum', sum);
+//     num++;
+// }
+
+
+// let num = 1;
+// while (num <= 60) {
+//     console.log('Hello from bangladesh', num);
+//     num++;
+// }
+
+
+// let num = 1;
+// while (num <= 10) {
+//     console.log(num);
+//     if (num % 2 === 0) {
+//         console.log('even number', num);
+//     }
+//     num++;
+// }
+
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     if (i % 2 === 1) {
+//         console.log('odd number', i);
+//     }
+//     i++;
+// }
+
+
+// let num = 0;
+// while (num <= 10) {
+//     console.log('even number', num)
+//     num = num + 2;
+// }
+
+
+// let num = 1;
+// while (num <= 20) {
+//     console.log('odd number', num);
+//     num = num + 2;
+// }
+
+
+
+// for (let i = 0; i <= 10; i = i + 2) {
+//     console.log('even number', i);
+// }
+
+
+// for (let num = 1; num <= 10; num = num + 2) {
+//     console.log('odd number', num);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log('My name is Rakibul Hasan', i);
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     sum = sum + i;
+// }
+// console.log('sum of number', sum);
+
+
+// let i = 10;
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// for (let i = 10; i >= 0; i--) {
+//     console.log(i);
+// }
+
+// let i = 51;
+// while (i >= 40) {
+//     console.log(i);
+//     i = i - 2;
+// }
+
+
+// for (let i = 31; i >= 20; i = i - 2) {
+//     console.log(i)
+// }
+
+
+// let i = 81;
+// let sum = 0;
+// while (i <= 131) {
+//     console.log('odd number:', i);
+//     sum = sum + i;
+//     console.log('sum of odd number:', sum);
+//     i = i + 2;
+// }
+
+
+// let num = 206;
+// let sum = 0;
+// while (num <= 311) {
+//     console.log('even number', num);
+//     sum = sum + num;
+//     console.log('sum of even number', sum);
+//     num = num + 2;
+// }
+
+
+// let sum = 0;
+// for (let i = 20; i <= 30; i = i + 2) {
+//     console.log('even number', i);
+//     sum = sum + i;
+// }
+// console.log('sum of even number', sum);
+
+
+// let sum = 0;
+// for (let num = 1; num <= 10; num = num + 2) {
+//     console.log('odd number', num);
+//     sum = sum + num;
+// }
+// console.log('sum of odd number', sum);
+
+
+// let n = 5;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(n + " * " + i + " = " + n * i);
+// }
+
+
+// let sum = 7;
+// for (let i = 5; i <= 10; i++) {
+//     console.log(sum + '*' + i + '=' + sum * i);
+// }
+
+
+// let i = 1;
+// let num = 9;
+// while (i <= 10) {
+//     console.log(num + '*' + i + '=' + num * i);
+//     i++;
+// }
+
+
+// let i = 1;
+// let n = 10;
+// while (i <= 10) {
+//     console.log(n + '*' + i + '=' + n * i);
+//     i++;
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let i = 0;
+// while (i <= 10) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// for (let i = 1; i <= 25; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// for (let i = 1; i <= 24; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// let i = 1;
+// while (i <= 40) {
+//     if (i % 4 === 0) {
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+
+// for (let i = 1; i <= 9; i++) {
+//     if (i % 3 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+
+// let total = 0;
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         total = total + i;
+//         console.log('total', total);
+//     }
+// }
+// console.log('total of the num:', total);
+
+
+
+// let i = 1
+// let total = 0;
+// while (i <= 30) {
+//     if (i % 3 === 0) {
+//         console.log('total', i)
+//         total = total + i;
+//         console.log('total of num:', total);
+//     }
+//     i++;
+// }
+
+
+// let i = 1;
+// let total = 0;
+// while (i <= 50) {
+//     if (i % 5 === 0) {
+//         console.log('total:', i);
+//         total = total + i
+//         console.log('total of num:', total)
+//     }
+//     i++;
+// }
+
+
+
+// let total = 0;
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 4 === 0) {
+//         console.log('total:', i);
+//         total = total + i;
+//         console.log('total of num:', total)
+//     }
+// }
+
+
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+//     if (i >= 10) {
+//         break;
+//     }
+// }
+
+
+// for (let i = 5; i <= 40; i++) {
+//     console.log(i);
+//     if (i >= 40) {
+//         break;
+//     }
+// }
+
+
+// for (let i = 1; i <= 15; i++) {
+//     console.log(i);
+//     if (i >= 10) {
+//         break;
+//     }
+// }
+
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     if (i >= 5) {
+//         break;
+//     }
+//     i++;
+// }
+
+
+// let i = 30;
+// while (i >= 20) {
+//     console.log(i);
+//     if (i <= 25) {
+//         break;
+//     }
+//     i--;
+// }
+
+
+// for (let i = 50; i >= 30; i--) {
+//     console.log(i)
+//     if (i <= 40) {
+//         break;
+//     }
+// }
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let i = 1;
+// while (i <= 10) {
+//     i++;
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+// let i = 21;
+// while (i >= 15) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// for (let i = 21; i >= 15; i--) {
+//     console.log(i);
+// }
+
+
+
+// let i = 1;
+// while (i <= 200) {
+//     console.log(i);
+//     if (i >= 100) {
+//         break;
+//     }
+//     i++;
+// }
+
+
+// for (let i = 1; i <= 200; i++) {
+//     console.log(i);
+//     if (i >= 100) {
+//         break;
+//     }
+// }
+
+
+
+// let i = 1;
+// let sum = 0;
+// while (i <= 200) {
+//     console.log('total:', i);
+//     if (i >= 100) {
+//         break;
+//     }
+//     sum = sum + i;
+//     console.log(sum);
+//     i++;
+// }
+
+
+// let sum = 0;
+// for (let i = 1; i <= 200; i++) {
+//     console.log('total:', i)
+//     if (i >= 100) {
+//         break;
+//     }
+//     sum = sum + i;
+//     console.log('sum of num:', sum);
+// }
+
+
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i = 55; i <= 85; i++) {
+//     // console.log(i);
+//     if (i % 5 === 0) {
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
+// let i = 1;
+// while (i <= 40) {
+//     i++;
+//     if (i % 5 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// let i = 1;
+// while (i <= 40) {
+//     i++;
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
