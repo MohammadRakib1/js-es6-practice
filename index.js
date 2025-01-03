b485b711c6b69ea8b3cbf3c2cb04e1be5c2145d6
MohammadOfficials <mohammadofficial4@gmail.com>
2025-01-03 12:05:27 +0600

    js-string-objects-practice

--- index.js
+++ index.js
@@ -1,447 +1,223 @@
-// let i = 1;
-// let sum = 0;
-// while (i <= 10) {
-//     console.log(i);
-//     sum = sum + i;
-//     console.log('sum', sum);
-//     i++;
+//-------------------toLowerCase() and toUpperCase()--------------------
+// let subject = 'Chemistry';
+// let book = 'chemistry';
+// if (subject.toLowerCase() === book) {
+//     console.log('I am reading book');
+// }
+// else {
+//     console.log('I am not reading book');
 // }
 
 
-// let num = 11;
-// let sum = 0;
-// while (num <= 20) {
-//     console.log(num);
-//     sum = sum + num;
-//     console.log('sum', sum);
-//     num++;
+//-----------------------trim(), trimEnd(), trimStart()--------------------
+// let drink = 'water ';
+// let liquid = ' water';
+// if (drink.trimEnd() === liquid.trimStart()) {
+//     console.log('solid water');
+// }
+// else {
+//     console.log('wrong water');
 // }
 
 
-// let num = 1;
-// while (num <= 60) {
-//     console.log('Hello from bangladesh', num);
-//     num++;
+//---------------slice(2, 5)--------------------
+// const country = 'Bangladesh';
+// const part = country.slice(3, 7);
+// console.log(part);
+
+
+// --------------------split()------------------------
+// const sentence = 'I am a good person';
+// console.log(sentence.split(''));
+// const friendsStr = 'Rakib, Tareq, Saimon, Tanvir, Al-Hasan, Al-Hossen, Hasnat';
+// const friends = friendsStr.split(',');
+// console.log(friends);
+
+
+//-------------------------join()-----------------------------
+// const familyMember = ['Rakib', 'Tareq', 'Saimon', 'Tanvir', 'Al-Hasan', 'Al-Hossen', 'Hasnat'];
+// console.log(familyMember.join());
+// console.log(familyMember.join('|'));
+// console.log(familyMember.join('-'));
+
+
+//-----------------------concat and includes---------------------------
+// const first = 'Mohammad';
+// const last = 'Rakib';
+// const fullName = first.concat(' ').concat(last);
+// console.log(fullName.includes('c'));
+
+
+//-----------------------reverse string----------------------
+// let sentence = 'I am a web developer';
+// let reverse = '';
+// for (let letter of sentence) {
+//     // console.log(letter);
+//     reverse = letter + reverse;
 // }
+// console.log(reverse);
 
-
-// let num = 1;
-// while (num <= 10) {
-//     console.log(num);
-//     if (num % 2 === 0) {
-//         console.log('even number', num);
-//     }
-//     num++;
+// let country = 'Bangladesh is my country';
+// let rev = '';
+// for (letter of country) {
+//     // console.log(letter);
+//     rev = letter + rev;
 // }
+// console.log(rev);
+
+// let home = 'chittagong';
+// let rev = home.split('').reverse('').join('');
+// console.log(rev);
 
 
-// let i = 1;
-// while (i <= 10) {
-//     console.log(i);
-//     if (i % 2 === 1) {
-//         console.log('odd number', i);
-//     }
-//     i++;
+//---------------object declare------------------
+// const student = {
+//     name: 'Mohammad Rakib',
+//     class: 12,
+//     subject: 'chemistry',
 // }
+// console.log(student.name);
+
+// const nam = student.name;
+// console.log(nam);
 
 
-// let num = 0;
-// while (num <= 10) {
-//     console.log('even number', num)
-//     num = num + 2;
+// const bottle = {
+//     price: 230,
+//     color: 'yello',
+//     isClean: false
 // }
+// console.log(bottle['color']);
+// const dam = bottle['price'];
+// console.log(dam);
 
 
-// let num = 1;
-// while (num <= 20) {
-//     console.log('odd number', num);
-//     num = num + 2;
+// const laptop = {
+//     price: 100000,
+//     color: 'gray',
+//     brand: 'lenovo',
+//     ram: '8gb',
+//     ssd: '1TB',
+//     'order laptop': ['hp', 'asus', 'dell', 'apple', 'lenovo']
 // }
+// laptop.price = 200000;
+// laptop['order laptop'] = ['apple', 'microsoft', 'asusbook']
+// console.log(laptop)
+// console.log(laptop['order laptop']);
+// const laptops = 'brand';
+// laptop[laptops] = 'apple';
+// console.log(laptop)
 
 
-
-// for (let i = 0; i <= 10; i = i + 2) {
-//     console.log('even number', i);
+// const mobile = {
+//     brand: 'apple',
+//     price: 100000,
+//     color: 'black',
+//     processor: 'octa-core',
+//     storage: 128,
+//     ram: 8,
 // }
+// const key = Object.keys(mobile);
+// console.log(key);
+// const value = Object.values(mobile);
+// console.log(value);
+// console.log(mobile);
 
 
-// for (let num = 1; num <= 10; num = num + 2) {
-//     console.log('odd number', num);
-// }
-
-
-// for (let i = 1; i <= 10; i++) {
-//     console.log('My name is Rakibul Hasan', i);
-// }
-
-// let sum = 0;
-// for (let i = 1; i <= 10; i++) {
-//     console.log(i);
-//     sum = sum + i;
-// }
-// console.log('sum of number', sum);
-
-
-// let i = 10;
-// while (i >= 0) {
-//     console.log(i);
-//     i--;
-// }
-
-
-// for (let i = 10; i >= 0; i--) {
-//     console.log(i);
-// }
-
-// let i = 51;
-// while (i >= 40) {
-//     console.log(i);
-//     i = i - 2;
-// }
-
-
-// for (let i = 31; i >= 20; i = i - 2) {
-//     console.log(i)
-// }
-
-
-// let i = 81;
-// let sum = 0;
-// while (i <= 131) {
-//     console.log('odd number:', i);
-//     sum = sum + i;
-//     console.log('sum of odd number:', sum);
-//     i = i + 2;
-// }
-
-
-// let num = 206;
-// let sum = 0;
-// while (num <= 311) {
-//     console.log('even number', num);
-//     sum = sum + num;
-//     console.log('sum of even number', sum);
-//     num = num + 2;
-// }
-
-
-// let sum = 0;
-// for (let i = 20; i <= 30; i = i + 2) {
-//     console.log('even number', i);
-//     sum = sum + i;
-// }
-// console.log('sum of even number', sum);
-
-
-// let sum = 0;
-// for (let num = 1; num <= 10; num = num + 2) {
-//     console.log('odd number', num);
-//     sum = sum + num;
-// }
-// console.log('sum of odd number', sum);
-
-
-// let n = 5;
-// for (let i = 1; i <= 10; i++) {
-//     console.log(n + " * " + i + " = " + n * i);
-// }
-
-
-// let sum = 7;
-// for (let i = 5; i <= 10; i++) {
-//     console.log(sum + '*' + i + '=' + sum * i);
-// }
-
-
-// let i = 1;
-// let num = 9;
-// while (i <= 10) {
-//     console.log(num + '*' + i + '=' + num * i);
-//     i++;
-// }
-
-
-// let i = 1;
-// let n = 10;
-// while (i <= 10) {
-//     console.log(n + '*' + i + '=' + n * i);
-//     i++;
-// }
-
-
-// for (let i = 0; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
+// const college = {
+//     name: 'vnc',
+//     class: ['10', '11', '12'],
+//     events: ['science fair', 'bijoy dibos', '21 Feb'],
+//     unique: {
+//         color: 'white',
+//         result: {
+//             gpa: 5,
+//             merit: 'top level'
+//         }
 //     }
 // }
+// console.log(college.events[2]);
+// console.log(college.unique.result.merit)
+// college.unique.result.merit = 'top higher level';
+// college.events[2] = '16 December';
+// college.events[1] = '16 dec';
+// delete college.name;
+// console.log(college)
 
 
-// let i = 0;
-// while (i <= 10) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//     }
-//     i++;
+//-------------------------(for in) only usege object---------------------
+// const mobile = {
+//     brand: 'Redmi',
+//     price: 20000,
+//     ram: '4gb',
+//     storage: 128,
+//     color: 'black',
+//     camera: '50mp',
+//     displaySize: '12 inc'
 // }
 
-
-// for (let i = 1; i <= 25; i++) {
-//     if (i % 5 === 0) {
-//         console.log(i);
-//     }
+// for (const key in mobile) {
+//     // console.log(mobile[key]);
 // }
 
+// const keys = Object.keys(mobile);
+// console.log(keys);
 
-// for (let i = 1; i <= 24; i++) {
-//     if (i % 3 === 0) {
-//         console.log(i);
-//     }
-// }
-
-
-// let i = 1;
-// while (i <= 40) {
-//     if (i % 4 === 0) {
-//         console.log(i);
-//     }
-//     i++;
-// }
-
-
-// for (let i = 1; i <= 9; i++) {
-//     if (i % 3 === 0) {
-//         console.log(i);
-//     }
+// for (const key of keys) {
+//     console.log(key, ':', mobile[key]);
 // }
 
 
 
-// let total = 0;
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//         total = total + i;
-//         console.log('total', total);
-//     }
+//--------------------------------------------------- JS Task ------------------------------------------
+// let myObjects = {
+//     name: 'Tanvirul Islam',
+//     age: 25,
+//     city: 'Dhaka',
+//     isStudent: true
 // }
-// console.log('total of the num:', total);
 
-
-
-// let i = 1
-// let total = 0;
-// while (i <= 30) {
-//     if (i % 3 === 0) {
-//         console.log('total', i)
-//         total = total + i;
-//         console.log('total of num:', total);
-//     }
-//     i++;
+// for (let key in myObjects) {
+//     console.log('key:', key, '| type:', typeof myObjects[key]);
 // }
 
 
-// let i = 1;
-// let total = 0;
-// while (i <= 50) {
-//     if (i % 5 === 0) {
-//         console.log('total:', i);
-//         total = total + i
-//         console.log('total of num:', total)
-//     }
-//     i++;
+// const color = {
+//     red: '#ff0000',
+//     green: '#00ff00',
+//     blue: '#000ff',
+//     'golden rod': '#daa520'
 // }
+// const golden = color['golden rod'];
+// console.log(golden);
 
 
+// const car = {
+//     make: 'Toyota',
+//     model: 'Corolla',
+//     year: 2020
+// }
+// car['passengerCapacity'] = 5;
+// console.log(car)
 
-// let total = 0;
-// for (let i = 1; i <= 40; i++) {
-//     if (i % 4 === 0) {
-//         console.log('total:', i);
-//         total = total + i;
-//         console.log('total of num:', total)
+
+// const student = {
+//     name: 'Mohammad Rakib',
+//     id: 5421,
+//     physics: {
+//         subject: 'HSC physics',
+//         author: 'Tarek Hasan',
+//         marks: 30
 //     }
 // }
+// console.log(student.physics.marks);
 
 
-
-// for (let i = 0; i <= 20; i++) {
-//     console.log(i);
-//     if (i >= 10) {
-//         break;
-//     }
+// let student = {
+//     name: 'Alam',
+//     age: 23,
+//     city: 'chottogram',
+//     isStudent: true
 // }
-
-
-// for (let i = 5; i <= 40; i++) {
-//     console.log(i);
-//     if (i >= 40) {
-//         break;
-//     }
-// }
-
-
-// for (let i = 1; i <= 15; i++) {
-//     console.log(i);
-//     if (i >= 10) {
-//         break;
-//     }
-// }
-
-
-// let i = 1;
-// while (i <= 10) {
-//     console.log(i);
-//     if (i >= 5) {
-//         break;
-//     }
-//     i++;
-// }
-
-
-// let i = 30;
-// while (i >= 20) {
-//     console.log(i);
-//     if (i <= 25) {
-//         break;
-//     }
-//     i--;
-// }
-
-
-// for (let i = 50; i >= 30; i--) {
-//     console.log(i)
-//     if (i <= 40) {
-//         break;
-//     }
-// }
-
-
-
-// for (let i = 1; i <= 10; i++) {
-//     if (i % 2 === 1) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-
-// for (let i = 1; i <= 20; i++) {
-//     if (i % 2 === 0) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-
-// let i = 1;
-// while (i <= 10) {
-//     i++;
-//     if (i % 2 === 1) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-
-
-// let i = 21;
-// while (i >= 15) {
-//     console.log(i);
-//     i--;
-// }
-
-
-// for (let i = 21; i >= 15; i--) {
-//     console.log(i);
-// }
-
-
-
-// let i = 1;
-// while (i <= 200) {
-//     console.log(i);
-//     if (i >= 100) {
-//         break;
-//     }
-//     i++;
-// }
-
-
-// for (let i = 1; i <= 200; i++) {
-//     console.log(i);
-//     if (i >= 100) {
-//         break;
-//     }
-// }
-
-
-
-// let i = 1;
-// let sum = 0;
-// while (i <= 200) {
-//     console.log('total:', i);
-//     if (i >= 100) {
-//         break;
-//     }
-//     sum = sum + i;
-//     console.log(sum);
-//     i++;
-// }
-
-
-// let sum = 0;
-// for (let i = 1; i <= 200; i++) {
-//     console.log('total:', i)
-//     if (i >= 100) {
-//         break;
-//     }
-//     sum = sum + i;
-//     console.log('sum of num:', sum);
-// }
-
-
-
-// for (let i = 1; i <= 100; i++) {
-//     if (i % 2 === 0) {
-//         console.log(i);
-//         break;
-//     }
-// }
-
-
-
-// for (let i = 1; i <= 40; i++) {
-//     if (i % 2 === 1) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-
-// for (let i = 55; i <= 85; i++) {
-//     // console.log(i);
-//     if (i % 5 === 0) {
-//         continue;
-//     }
-//     console.log(i)
-// }
-
-
-// let i = 1;
-// while (i <= 40) {
-//     i++;
-//     if (i % 5 === 0) {
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-
-// let i = 1;
-// while (i <= 40) {
-//     i++;
-//     if (i % 2 === 1) {
-//         continue;
-//     }
-//     console.log(i);
-// }
+// let checkProperty = Object.keys(student).length;
+// console.log(checkProperty);
